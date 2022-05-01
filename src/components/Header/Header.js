db4b535799e784befe0a3c751003d5d481bca01d
werz <78509039+0xWerz@users.noreply.github.com>
2022-05-01 06:05:55 +0100

    urlpreview

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,9 +12,22 @@ const Header = () =>  (
   <Container>
       <Head>
         <title>Werz | Cybersecurity</title>
-        <meta name="description" content="Posts about Cybersecurity"></meta>
-        <meta property="og:description" content="Posts about Cybersecurity"></meta>
-        <meta property="og:image" content="/images/bmo.jpg"></meta>
+        <meta name="description" content="Posts about Cybersecurity, and more"></meta>
+
+        <meta name="author" content="Werz"></meta>
+
+        <meta property="og:type" content="website"></meta>
+        <meta property="og:locale" content="en_US"></meta>
+        <meta property="og:site_name" content="Werz | Cybersecurity"></meta>
+        <meta property="og:title" content="         Werz      Cybersecurity"></meta>
+        <meta property="og:url" content="https://0xwerz.github.io/"></meta>
+
+        <meta name="robots" content="index, follow"></meta>
+
+
+        <meta property="og:description" content="Posts about Cybersecurity, And more"></meta>
+        <meta property="og:image" content="https://0xwerz.github.io/images/bmo.jpg"></meta>
+        <link rel="canonical" href="https://0xwerz.github.io/"></link>
       </Head>
     <Div1>
       <Link href="/">
